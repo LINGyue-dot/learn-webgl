294baa1424cd15d543d69d8401e0978fa85cc8a1
shunze.chen <qianlonwork@outlook.com>
2023-07-26 14:43:20 +0800

    feat: draw double rect

--- webgl/gettingstarted/antoher/index.js
+++ webgl/gettingstarted/antoher/index.js
@@ -2,7 +2,12 @@ function createShader(gl, type, source) {
   const shader = gl.createShader(type);
   gl.shaderSource(shader, source);
   gl.compileShader(shader);
-  return shader;
+  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
 }
 
 function createProgram(gl, vertexShader, fragmentShader) {
@@ -10,7 +15,13 @@ function createProgram(gl, vertexShader, fragmentShader) {
   gl.attachShader(program, vertexShader);
   gl.attachShader(program, fragmentShader);
   gl.linkProgram(program);
-  return program
+  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
 }
 
 function main() {
@@ -32,16 +43,23 @@ function main() {
   // 需要传递 position 参数
   const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 
+  const resolutionUniformLocation = gl.getUniformLocation(
+    program,
+    "u_resolution"
+  );
+
   const positionBuffer = gl.createBuffer();
 
   // 将 buffer 指向 gl.ARRAY_BUFFER
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-  const positions = [-1, -1, 0, 1, 1, -1];
+  const positions = [10, 20, 80, 20, 10, 30, 10, 30, 80, 20, 80, 30];
 
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
   gl.useProgram(program);
+
+  gl.uniform2f(resolutionUniformLocation, 500, 500);
 
   // TODO: remove ? 将 buffer 指向 gl.ARRAY_BUFFER
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -68,8 +86,17 @@ function main() {
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   const primitiveType = gl.TRIANGLES;
-  const count = 3; // 顶点着色器执行几次
+  const count = positions.length / 2; // 顶点着色器执行几次
   gl.drawArrays(primitiveType, 0, count);
+
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      110, 120, 180, 120, 110, 130, 110, 130, 180, 120, 180, 130,
+    ]),
+    gl.STATIC_DRAW
+  );
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
 }
 
 main();
